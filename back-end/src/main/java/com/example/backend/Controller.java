e6bcc622460356dbb1bfcd39940da0b9d7de414f
QuentinC26 <q.zuzlewski@gmail.com>
2026-06-15 23:59:45 +0200

    i add the api for application with annotations crossorigins, restconstroller, requestmapping and httprequest

---
+++ back-end/src/main/java/com/example/backend/Controller.java
@@ -0,0 +1,51 @@
+package com.example.backend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Pour éviter les erreurs Cors
+@CrossOrigin(origins = "http://localhost:4200") 
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    private List<DrawData> datadrawList = new ArrayList<>();
+
+    @GetMapping("/datadraw")
+    public List<DrawData> getAlldatadraw() {
+        return datadrawList;
+    }
+
+    @PostMapping("/datadraw")
+    public String addDatadraw(@RequestBody DrawData datadraw) {
+        datadrawList.add(datadraw);
+        return "Données insérées avec succès";
+    }
+
+    @PutMapping("/datadraw/{id}")
+    public String updateDatadraw(@PathVariable int id, @RequestBody DrawData updatedDataDraw) {
+        for (DrawData datadraw : datadrawList) {
+            if (datadraw.getId() == id) {
+                datadraw.setName(updatedDataDraw.getName());
+                return "Données mises à jour avec succès";
+            }
+        }
+        return "Détail introuvable !";
+    }
+
+    @DeleteMapping("/datadraw/{id}")
+    public String deleteDataDraw(@PathVariable int id) {
+        datadrawList.removeIf(datadraw -> datadraw.getId() == id);
+        return "Données supprimées avec succès";
+    }
+  }
