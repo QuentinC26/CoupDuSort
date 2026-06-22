7fe8f39ccc63c876dc27019beae0defb487385b7
QuentinC26 <q.zuzlewski@gmail.com>
2026-06-22 12:38:47 +0200

    i add creer list and fairetirage class

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,12 +1,45 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+
+export class App{
+
+  newParticipants = '';
+  participants: string[] = [];
+  addSucess = '';
+
+  constructor(private http: HttpClient) {}
+
+  creerListe() {
+    this.participants = this.newParticipants
+      .split('\n')
+      .map(p => p.trim())
+      .filter(p => p.length > 0);
+      
+      this.faireTirage();
+      this.addSucess = `${this.participants.length} participants ajoutés !`;
+  }
+
+  faireTirage() {
+    this.http.post(
+    'http://localhost:8080/api/datadraw',
+    {
+      participants: this.participants
+    }
+  ).subscribe(result => {
+    console.log(result);
+  });
 }
+}
+
+bootstrapApplication(App);
