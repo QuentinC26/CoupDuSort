d2b95eb5570e906402a1faf01cefa1cd3c526661
QuentinC26 <q.zuzlewski@gmail.com>
2026-07-27 19:57:48 +0200

    i add the hat logic

--- back-end/src/main/java/com/example/backend/Participant.java
+++ back-end/src/main/java/com/example/backend/Participant.java
@@ -4,10 +4,12 @@ public class Participant {
 
     private int id;
     private String name;
+    private Integer hatId;
 
-    public Participant(int id, String name) {
+    public Participant(int id, String name, Integer hatId) {
         this.id = id;
         this.name = name;
+        this.hatId = hatId;
     }
 
     // Constructeur vide nécessaire pour que Spring Boot (via Jackson) puisse créer automatiquement 
@@ -30,4 +32,36 @@ public class Participant {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Integer getHatId() {
+        return hatId;
+    }
+
+    public void setHatId(Integer hatId) {
+        this.hatId = hatId;
+    }
+
+    // Remplace une méthode qui existe déjà dans une classe parent.
+    @Override
+    public boolean equals(Object obj) {
+
+    // Vérifie si c'est exactement le même objet
+    if (this == obj) {
+        return true;
+    }
+
+    // Vérifie que l'objet est bien un Participant
+    if (!(obj instanceof Participant)) {
+      return false;
+    }
+
+    Participant other = (Participant) obj;
+    return this.id == other.id;
+  }
+
+    @Override
+    // Permet d'identifier un participant avec son id dans les collections Java comme HashSet ou HashMap.
+    public int hashCode() {
+      return Integer.hashCode(id);
+  }
 }
