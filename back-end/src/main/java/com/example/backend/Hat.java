d508152cedc9e946ab9ed93428c39dc9452dc798
QuentinC26 <q.zuzlewski@gmail.com>
2026-07-27 19:58:18 +0200

    i add the hat class

---
+++ back-end/src/main/java/com/example/backend/Hat.java
@@ -0,0 +1,31 @@
+package com.example.backend;
+
+public class Hat {
+
+    private int id;
+    private String name;
+
+    public Hat() {
+    }
+
+    public Hat(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
