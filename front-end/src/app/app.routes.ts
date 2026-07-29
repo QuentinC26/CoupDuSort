cabfee8b9bcbf846f2f961dcba77f2fe60332567
QuentinC26 <q.zuzlewski@gmail.com>
2026-07-29 17:34:11 +0200

    i add all routes for the navigation of coup du sort

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Draw } from './draw';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Draw
+  },
+  {
+    path: 'casino',
+    component: Draw
+  },
+  {
+    path: 'noel',
+    component: Draw
+  },
+  {
+    path: 'halloween',
+    component: Draw
+  },
+  {
+    path: 'sportif',
+    component: Draw
+  }
+];
